a70f9a098e180e750159c32b7a25e29b4de27616
Raluca <raluca.brasoveanu@tecknoworks.com>
2017-08-14 16:20:44 +0300

    commit restaurant

---
+++ Green/Entities/Enums/RestaurantType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Green.Entities.Enums
+{
+    public enum RestaurantType
+    {
+        Vegetarian,
+        Vegan,
+        Traditional,
+        Backery,
+        Pastry
+    }
+}
---
+++ Green/Entities/Restaurant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Green.Entities
+{
+    public class Restaurant
+    {
+        public string id { get; set; }
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public Enums.RestaurantType Type { get; set; }
+        public int MaxPrice { get; set; }
+
+    }
+}
