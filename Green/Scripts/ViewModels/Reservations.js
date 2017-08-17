eb6e2eecc5d1025bd5c4ed3d1086feb782564c75
Adriana <adriana.moisil@gmail.com>
2017-08-18 00:17:30 +0300

    validate Reservations

--- Green/Scripts/ViewModels/Reservations.js
+++ Green/Scripts/ViewModels/Reservations.js
@@ -13,7 +13,7 @@
     // validation warnings
     self.warningRestaurantId = ko.observable();
     self.warningClientName = ko.observable();
-   // self.warningReservationDate = ko.observable();
+    self.warningReservationDate = ko.observable();
     self.warningSeats = ko.observable();
 
     self.details = function (data) {
@@ -22,6 +22,10 @@
         self.ClientName(data.ClientName);
         self.ReservationDate(data.ReservationDate);
         self.Seats(data.Seats);
+        self.warningRestaurantId(null);
+        self.warningClientName(null);
+         self.warningReservationDate(null);
+        self.warningSeats(null);
     };
 
     self.add = function () {
@@ -30,6 +34,10 @@
         self.ClientName(null);
         self.ReservationDate(null);
         self.Seats(null);
+        self.warningRestaurantId(null);
+        self.warningClientName(null);
+        self.warningReservationDate(null);
+        self.warningSeats(null);
     }
 
 
@@ -54,27 +62,25 @@
     };
 
     self.save = function () {
-        var url = '/Reservations/Save';
-        // validation
-        var valid = true;
-        if (self.RestaurantId._latestValue == null) {
-            self.warningRestaurantId("Please select a restaurant!\n");
-            valid = false;
-        }
-        if (self.ClientName._latestValue == null) {
-            self.warningClientName("Please enter your name!\n");
-            valid = false;
-        }
-        if (self.Seats._latestValue <= 0) {
-            self.warningSeats("Please select the number of seats!\n");
-            valid = false;
-        }
-       
-        if (!valid)
+        var attrDataDismiss = document.createAttribute("data-dismiss");
+        attrDataDismiss.value = "modal";
+        if (!self.validate()) {
+            try {
+                document.getElementById("OKButton").attributes.removeNamedItem("data-dismiss");
+            } catch (Exception) {
+            }
             return;
-        self.warningRestaurantId("");
-        self.warningClientName("");
-        self.warningSeats("");
+        }
+        try {
+            document.getElementById("OKButton").attributes.setNamedItem(attrDataDismiss);
+        } catch (Exception) {
+        }
+
+        var url = '/Reservations/Save';
+        self.warningRestaurantId(null);
+        self.warningClientName(null);
+        self.warningReservationDate(null);
+        self.warningSeats(null);
         var reservation = JSON.stringify({
             Id: self.Id(),
             RestaurantId: self.RestaurantId(),
@@ -114,4 +120,48 @@
             }
         });
     };
+
+    self.validate = function () {
+        var valid = true;
+        if (self.nullOrEmpty(self.RestaurantId._latestValue)) {
+            self.warningRestaurantId("Please select a restaurant!\n");
+            valid = false;
+        }
+        else {
+            self.warningRestaurantId(null);
+        }
+
+        if (self.nullOrEmpty(self.ClientName._latestValue)) {
+            self.warningClientName("Please enter your name!\n");
+            valid = false;
+        }
+        else {
+            self.warningClientName(null);
+        }
+
+        if (self.nullOrEmpty(self.ReservationDate._latestValue) || self.ReservationDate._latestValue == "Invalid date" || self.ReservationDate._latestValue[0] == "/") {
+            self.warningReservationDate("Please select the date!\n");
+            valid = false;
+        }
+        else {
+            self.warningReservationDate(null);
+        }
+
+        if (self.nullOrEmpty(self.Seats._latestValue) || self.Seats._latestValue <= 0) {
+            self.warningSeats("Please select the number of seats!\n");
+            valid = false;
+        }
+        else {
+            self.warningSeats(null);
+        }
+
+        return valid;
+    }
+
+    self.nullOrEmpty = function (data) {
+        if (data == null || data == "") {
+            return true;
+        }
+        return false;
+    }
 }
