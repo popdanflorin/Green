c09689f3498a7b7d4caaba9ea7fa6bcf1ca4f67c
Adriana <adriana.moisil@tecknoworks.com>
2017-08-17 11:30:27 +0300

    added files

---
+++ Green/Services/ReservationQueryService.cs
@@ -0,0 +1,18 @@
+﻿using Green.Entities;
+using Green.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Green.Services
+{
+    public class ReservationQueryService
+    {
+        private ApplicationDbContext ctx = new ApplicationDbContext();
+        public List<Reservation> GetReservations()
+        {
+            return ctx.Reservations.ToList();
+        }
+    }
+}
