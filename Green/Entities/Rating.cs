c8e54f755ff85a91394d442cba4644f258382d2b
Adriana <adriana.moisil@tecknoworks.com>
2017-08-23 09:17:14 +0300

    add ratings

---
+++ Green/Entities/Rating.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Green.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Green.Entities
+{
+    public class Rating
+    {
+        public string Id { get; set; }
+        public string ClientId { get; set; }
+        public string RestaurantId { get; set; }
+        public int Value { get; set; }
+        [ForeignKey("ClientId")]
+        public virtual ApplicationUser Client { get; set; }
+        [ForeignKey("RestaurantId")]
+        public virtual Restaurant Restaurant { get; set; }
+    }
+}
