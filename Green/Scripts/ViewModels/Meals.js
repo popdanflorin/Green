66d4f35204d1a3eaee53eb94e40d11d5210406d5
Adriana <adriana.moisil@tecknoworks.com>
2017-08-28 16:20:28 +0300

    get ingredients for meal

--- Green/Scripts/ViewModels/Meals.js
+++ Green/Scripts/ViewModels/Meals.js
@@ -13,9 +13,8 @@
     self.Description = ko.observable();
     self.Type = ko.observable();
     self.Rating = ko.observable();
-    self.Ingredient = "";
-    self.IngredientsName = ko.observable();
-    self.IngredientsId = [];
+    self.Ingredient = ko.observable();
+    self.Ingredients = ko.observable();
 
     //for search
     self.SearchName = ko.observable(false);
@@ -35,24 +34,14 @@
         self.Description(data.Description);
         self.Type(data.Type);
         self.Rating(data.Rating);
-
-        self.IngredientsId = [];
-        self.IngredientsName(null);
-        if (data.Ingredients != null) {
-            var tmp = data.Ingredients.split(",");
-            tmp.sort();
-            tmp.forEach(function (ingredient) {
-                self.Ingredient = ingredient;
-                self.IngredientsId.push(self.Ingredient);
-            });
-        }
-        self.getIngredientsName();
         self.Ingredient = null;
 
         self.warningName(null);
         self.warningDescription(null);
         self.warningType(null);
         self.warningIngredient(null);
+
+        self.getIngredients();
     };
 
     self.add = function () {
@@ -61,9 +50,8 @@
         self.Name(null);
         self.Description(null);
         self.Rating(null);
-        self.Ingredient = null;
-        self.IngredientsId = [];
-        self.IngredientsName(null);
+        self.Ingredient(null);
+        self.Ingredients(null);
 
         self.warningName(null);
         self.warningDescription(null);
@@ -79,27 +67,13 @@
         self.setOKButton(true);
 
         var url = '/Meals/Save';
-        var meal;
-        try {
-            meal = JSON.stringify({
+        var meal = JSON.stringify({
                 Id: self.Id(),
                 Name: self.Name(),
                 Description: self.Description(),
                 Type: self.Type(),
-                Rating: self.Rating(),
-                Ingredients: self.IngredientsId().join(",").toString()
+                Rating: self.Rating()
             });
-        }
-        catch (e) {
-            meal = JSON.stringify({
-                Id: self.Id(),
-                Name: self.Name(),
-                Description: self.Description(),
-                Type: self.Type(),
-                Rating: self.Rating(),
-                Ingredients: self.IngredientsId.join(",").toString()
-            });
-        }
         $.ajax(url, {
             type: "post",
             dataType: "json",
@@ -119,6 +93,26 @@
         var url = '/Meals/Delete';
         var meal = JSON.stringify({
             mealId: data.Id
+        });
+        $.ajax(url, {
+            type: "post",
+            dataType: "json",
+            contentType: "application/json; charset=utf-8",
+            data: meal,
+            success: function (data) {
+                console.log(data);
+                self.refresh();
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log(textStatus + ': ' + errorThrown);
+            }
+        });
+    };
+
+    self.deleteModal = function () {
+        var url = '/Meals/Delete';
+        var meal = JSON.stringify({
+            mealId: self.Id()
         });
         $.ajax(url, {
             type: "post",
@@ -159,59 +153,49 @@
         if (!self.validateIngredient())
             return;
 
-        var index = self.IngredientsId.findIndex(function (element) {
-            return element.valueOf() == self.Ingredient.valueOf();
-        });
-        if (index != -1) {
-            self.warningIngredient("Selected ingredient already exists in this meal!");
-            return;
-        }
+        //var index = self.IngredientsId.findIndex(function (element) {
+        //    return element.valueOf() == self.Ingredient.valueOf();
+        //});
+        //if (index != -1) {
+        //    self.warningIngredient("Selected ingredient already exists in this meal!");
+        //    return;
+        //}
 
-        self.warningIngredient(null);
-        self.IngredientsId.push(self.Ingredient);
-        self.getIngredientsName();
+        //self.warningIngredient(null);
+        //self.IngredientsId.push(self.Ingredient);
+        //self.getIngredientsName();
     };
 
     self.deleteIngredient = function () {
-        if (!self.validateIngredient())
-            return;
+        //if (!self.validateIngredient())
+        //    return;
 
-        var index = self.IngredientsId.findIndex(function (element) {
-            return element.valueOf() == self.Ingredient.valueOf();
-        });
-        if (index == -1) {
-            self.warningIngredient("Selected ingredient does not exist in this meal!");
-            return;
-        }
+        //var index = self.IngredientsId.findIndex(function (element) {
+        //    return element.valueOf() == self.Ingredient.valueOf();
+        //});
+        //if (index == -1) {
+        //    self.warningIngredient("Selected ingredient does not exist in this meal!");
+        //    return;
+        //}
 
-        self.warningIngredient(null);
-        self.IngredientsId.splice(index, 1);
-        self.getIngredientsName();
+        //self.warningIngredient(null);
+        //self.IngredientsId.splice(index, 1);
+        //self.getIngredientsName();
     }
 
-    self.getIngredientsName = function () {
-        var url = '/Meals/GetIngredientsName';
-        var foods = "";
-        try {
-            foods = JSON.stringify({
-                ingredientsId: ko.toJS(self.IngredientsId())
-            });
-        }
-        catch (e) {
-            foods = JSON.stringify({
-                ingredientsId: ko.toJS(self.IngredientsId)
-            });
-        }
-
+    self.getIngredients = function () {
+        var url = '/Meals/GetIngredients';
+        var meal = JSON.stringify({
+            mealId: self.Id()
+        });
         $.ajax(url, {
-            async: false,
             type: "post",
             dataType: "json",
             contentType: "application/json; charset=utf-8",
-            data: foods,
+            data: meal,
             success: function (data) {
+                self.Ingredients(data);
                 console.log(data);
-                self.IngredientsName(data);
             },
             error: function (jqXHR, textStatus, errorThrown) {
                 console.log(textStatus + ': ' + errorThrown);
@@ -268,25 +252,13 @@
     };
 
     self.validateIngredient = function () {
-        try {
-            if (self.Ingredient._latestValue != 0 && self.nullOrEmpty(self.Ingredient)) {
-                self.warningIngredient("No ingredient choosen!");
-                return false;
-            }
-            else {
-                self.warningIngredient(null);
-                return true;
-            }
+       if (self.Ingredient() != 0 && self.nullOrEmpty(self.Ingredient)) {
+            self.warningIngredient("No ingredient choosen!");
+            return false;
         }
-        catch (e) {
-            if (self.Ingredient != 0 && self.nullOrEmpty(self.Ingredient)) {
-                self.warningIngredient("No ingredient choosen!");
-                return false;
-            }
-            else {
-                self.warningIngredient(null);
-                return true;
-            }
+        else {
+            self.warningIngredient(null);
+            return true;
         }
     }
 
